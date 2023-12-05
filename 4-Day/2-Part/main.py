6fe69045d8367283cb6d99fe268c6bfb13364fd0
Jerome-Joseph-1 <jejoweb2020@gmail.com>
2023-12-06 02:11:18 +0530

    Removed Comments

--- 4-Day/2-Part/main.py
+++ 4-Day/2-Part/main.py
@@ -12,9 +12,7 @@ with open('input', 'r') as r:
     count, extra_copies = 0, 0
 
     for line in inp:
-        # print(line)
         extra_copies += extra[count]
-        # print(extra_copies)
         left, right = line.split('|')
         winning_cards = set(map(int, left.split(':')[1].split()))
         won = 0
@@ -25,10 +23,8 @@ with open('input', 'r') as r:
         if won:
             extra[count + 1] += total[count] * (extra_copies + 1)
             if count + 1 + won < len(extra): extra[won + count + 1] -= total[count] * (1 + extra_copies)
-            pass
         
         total[count] += extra_copies
 
-        # print(won, total, extra)
         count += 1
     print(sum(total[:-1]))
